fbd1ce95edac691826cbd95db23563ea1702d85a
Malo <malo.leroy2662@gmail.com>
2022-02-27 12:54:52 +0100

    Ajout d'un algorithme classique

--- Simulations/shor.py
+++ Simulations/shor.py
@@ -1,26 +1,26 @@
 import random
-from statistics import mean, median, stdev
 from time import time
 
 def main():
     # On prend un entier N dont on souhaite trouver la décomposition.
-    # Pour cela on utilise la fonction facteur, qui donne deux diviseurs de N.
     N = int(input('Entrez un nombre entier : '))
     assert N > 1
     t0 = time()
-    f = facteurs(N)
+    f = facteurs(N, deux_facteurs_shor)
     print(f"{N} = {' × '.join([str(i) for i in f])}")
     print(f'(calcul effectué en {(time() - t0):.2f} s)')
 
 # Renvoie une liste correspodant à la décomposition en facteurs premiers de N
-def facteurs(N):
+# O(C(N) * log N) avec C(N) la complexité de deux_facteurs
+def facteurs(N, deux_facteurs):
     n, p = deux_facteurs(N)
     if n == 1: # alors p = N est premier (?)
         return [N]
-    return facteurs(n) + facteurs(p)
+    return facteurs(n, deux_facteurs) + facteurs(p, deux_facteurs)
 
 # Revoie un couple de facteurs (n, p) tels que n <= p et N = n * p.
-def deux_facteurs(N):
+# O(R(N) * (log N)**2) avec R(N) la complexité de la recherche de période
+def deux_facteurs_shor(N):
     while True: # tant qu'on ne valide aucune des conditions de retour
         # 1. Prendre un nombre pseudo-aléatoire a < N
         a = random.randint(0, N-1)
@@ -37,8 +37,21 @@ def deux_facteurs(N):
             p = pgcd(a**(r//2)-1, N)
             return (min(n, p), max(n, p))
 
+def partie_entiere_racine_carree(N): # O(sqrt N)
+    r = 1
+    while r * r <= N:
+        r += 1
+    return r - 1
+
+def deux_facteurs_classique(N): # O(sqrt N)
+    for r in range(2, partie_entiere_racine_carree(N)):
+        if N % r == 0:
+            n, p = N // r, r
+            return (min(n, p), max(n, p))
+    return (1, N)
+
 # Renvoie le PGCD de a et de b en utilisant l'aglorithme d'Euclide.
-def pgcd(a, b):
+def pgcd(a, b): # O(log max(a, b))
     if a < 0 or b < 0:
         return pgcd(abs(a), abs(b))
     if a == b == 0:
