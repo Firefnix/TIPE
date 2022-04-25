f766aa5a3216c73e212254dc8bdf50b10ac9353a
Malo <malo.leroy2662@gmail.com>
2022-04-25 19:36:50 +0200

    L'animation se répète

--- Simulations/Intrication/animation mesure intrication.py
+++ Simulations/Intrication/animation mesure intrication.py
@@ -6,9 +6,7 @@ import random
 fig, ax = plt.subplots()
 plt.axis('square')
 
-photons = {} 
-for i in range(-10, 11, 2):
-    photons[i], = ax.plot([], [], ls = 'none', marker = 'o', color = 'purple')
+photons = {}
 
 plt.xlim(-10,10)
 plt.ylim(-10.5,10.5)
@@ -19,15 +17,20 @@ color = ['blue', 'green']
 
 plt.plot([0,0], [10,-10], color = 'black')
 
+def initialiser():
+    global photons
+    photons = {}
+    for i in range(-10, 11, 2):
+        photons[i], = ax.plot([], [], ls = 'none', marker = 'o', color = 'purple')
 
 def animate(i):
+    if i == 0:
+        initialiser()
     for c in photons:
         photons[c].set_data(X[i], c)
-
         if abs(photons[c].get_xdata()) <= 10**-1:
-                photons[c].set_color(random.choice(color))
-
+            photons[c].set_color(random.choice(color))
     return tuple(photons[c] for c in photons)
 
-ani = animation.FuncAnimation(fig, animate, frames=range(N), blit = True, interval = 5, repeat = False)
+ani = animation.FuncAnimation(fig, animate, frames=range(N), blit = True, interval = 5, repeat = True)
 plt.show()
