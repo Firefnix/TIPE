a34b2464797e436cf4cf2c5721508dad9ba07ef0
Firefnix <malo.leroy2662@gmail.com>
2022-05-26 23:49:28 +0200

    Oracle de somme (modules uniquement)
    Oracle par somme dans F2 (états propres et superposés), premiers tests (d'autres suivront)
    Correction de fautes de frappe

--- Simulations/Ordinateur/oracle.py
+++ Simulations/Ordinateur/oracle.py
@@ -1,26 +1,70 @@
-from qubit import bra, ket, Qudit
-from calcul import un, F2, int_vers_bin
+from qubit import bra, Qudit
+from calcul import zero, un, F2, int_vers_bin, sqrt, int_log2
 
 class Oracle:
     @staticmethod
-    def phase(f, taille_x):
-        return OracleDePhase(f, taille_x)
+    def phase(f):
+        return OracleDePhase(f)
 
     @staticmethod
-    def somme(f, taille_x):
-        return OracleDeSomme(f, taille_x)
+    def somme(f):
+        return OracleDeSomme(f)
 
 
 class OracleDePhase:
-    # taille_x: nombre de paramètres pris par f
-    def __init__(self, f, taille_x):
+    # f est une fonction de (F2)^n dans F2
+    def __init__(self, f):
         self.f = f
-        self.n = taille_x
 
     def __mul__(self, qudit):
-        assert qudit.dim == 2 ** self.n
+        n = int_log2(qudit.dim)-1
         r = Qudit(qudit.dim)
         for i in range(qudit.dim):
             r |= bra(i) | (bra(i) | qudit) * (-un) ** self.f(
-                *[F2(i) for i in int_vers_bin(i, taille=self.n)])
+                *[F2(i) for i in int_vers_bin(i, taille=n)])
         return r
+
+class OracleDeSomme:
+    # f est une fonction de (F2)^n dans F2
+    def __init__(self, f):
+        self.f = f
+
+    def _trouve_i0(self, psi):
+        for i in range(psi.dim):
+            if bra(i) | psi != zero:
+                return i
+
+    def _coord_y(self, c_non_nul, c_autre):
+        a = (c_autre / c_non_nul)
+        return sqrt((a*a + un).inverse())
+
+    def _trouve_alpha_beta(self, psi):
+        i0 = self._trouve_i0(psi)
+        i1 = (i0+1) % psi.dim if i0 % 2 == 0 else (i0-1) % psi.dim
+        c = self._coord_y(psi[i0], psi[i1])
+        if i0 % 2 == 0:
+            alpha = c
+            beta = sqrt(un - alpha*alpha)
+        else:
+            beta = c
+            alpha = sqrt(un - beta*beta)
+        return alpha, beta
+
+    def _trouve_coord_x(self, alpha, beta, psi):
+        n = psi.dim // 2
+        if alpha != zero:
+            a_inv = alpha.inverse()
+            return [a_inv * psi[2*i] for i in range(n)]
+        b_inv = beta.inverse()
+        return [b_inv * psi[2*i+1] for i in range(n)]
+
+    def __mul__(self, psi):
+        alpha, beta = self._trouve_alpha_beta(psi)
+        coord_x = self._trouve_coord_x(alpha, beta, psi)
+        res = Qudit(psi.dim)
+        n = int_log2(psi.dim // 2)-1
+        for j in range(2**n):
+            fx = self.f(*[F2(i) for i in int_vers_bin(j, taille=n)])
+            res |= bra(j, int(fx)) | alpha * coord_x[j]
+            res |= bra(j, int(fx + 1)) | beta * coord_x[j]
+        return res
--- Simulations/Ordinateur/oracle_test.py
+++ Simulations/Ordinateur/oracle_test.py
@@ -1,27 +1,42 @@
 from unittest import TestCase, main
 from calcul import un, F2, Matrice
+from qubit import bra, ket, Qudit
 from portes import H
-from oracle import *
+from oracle import OracleDePhase, OracleDeSomme
 
 
-class TestOracles:
+class TestOracles(TestCase):
     def f(self, x, y):
         assert isinstance(x, F2) and isinstance(y, F2)
         return x + y
 
     def test_phase_propre(self):
-        Uf = OracleDePhase(self.f, 2)
+        Uf = OracleDePhase(self.f)
         e1, e2 = ket(1, 0), ket(1, 0)
         e2 |= bra(1, 0) | (-un)
         assert e1 >> Uf == e2
 
     def test_phase_superposition(self):
-        Uf = OracleDePhase(self.f, 2)
+        Uf = OracleDePhase(self.f)
         e1 = ket(1, 0) >> (H**2)
         e2 = Qudit.matrice(
             (un / 2) * Matrice.int_tableau([[1], [-1], [1], [-1]])
         )
         assert e1 >> Uf == e2
+
+    def test_somme_propre(self):
+        Uf = OracleDeSomme(self.f)
+        e1 = ket(1, 1, 0)
+        e2 = ket(1, 0, 1)
+        e3 = ket(1, 0, 0)
+        assert e1 >> Uf == e1
+        assert e2 >> Uf == e3
+        assert e3 >> Uf == e2
+
+    def test_somme_superposition(self):
+        Uf = OracleDeSomme(self.f)
+        e0 = ket(0)**3 >> H**3
+        assert e0 >> Uf == e0
 
 
 if __name__ == '__main__':
