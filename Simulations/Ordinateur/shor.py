6c09de16bf0fd66cb550a58ee4161bdbfb816112
Firefnix <malo.leroy2662@gmail.com>
2022-06-01 20:14:08 +0200

    Shor (ver. quantique)

--- Simulations/Ordinateur/shor.py
+++ Simulations/Ordinateur/shor.py
@@ -1,33 +1,32 @@
+import random
+
+from calcul import pgcd, bin_vers_int
 from qubit import ket
-from portes import H, I
-from calcul import Matrice
+from portes import H, I, QFT
+from oracle import Oracle
+from fonctions_utiles import etat_de_base, H_option
 
 N = int(input('Entrez un nombre : '))
-N = 12
+
 m = 4
+M = 2 ** m
 Ei = ket(0) ** (2*m)
 G = (H ** m) @ (I ** m)
 
-def f(x: int):
-    return (a**x) % N
+def cree_f(a):
+    def f(b1, b2, b3, b4):
+        x = bin_vers_int(b1, b2, b3, b4)
+        return (a ** x) % N
+    return f
 
-def decomp(n: int, taille: int):
-    l = [ket(0) for i in range(taille)]
-    d = [int(i) for i in bin(n)[2:]]
-    d = [0] * (taille-len(d)) + d
-    q = None
-    for i in range(taille):
-        if d[i] == 1:
-            l[i] = ket(1)
-        if q is None:
-            q = l[i]
-        else:
-            q = q @ l[i]
-    return q
+def shor_quantique(m):
+    a = random.randint(0, N-1)
+    ...
+    U = Oracle.somme(cree_f(a))
+    e0 = etat_de_base(m, m , 0)
+    e1 = e0 >> H_option(2*m, debut=0, fin=m)
+    e2 = e1 >> U
+    e3 = e2 >> (QFT(m) @ I ** m)
+    return e3
 
-def recomp(q):
-    pass
-
-E = decomp(N, 2*m)
-
-print(G * E)
+print(shor_quantique(m))
