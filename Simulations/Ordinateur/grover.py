fc0f194fb0eeb0139e0bf9b11b75b85fd10670cc
Firefnix <malo.leroy2662@gmail.com>
2022-06-18 22:47:05 +0200

    Grover (en travaux)

--- Simulations/Ordinateur/grover.py
+++ Simulations/Ordinateur/grover.py
@@ -1,3 +1,4 @@
+from qubit import bra, ket
 from fonctions_utiles import etat_de_base, H_option
 from portes import H, I, PhaseCond
 from oracle import Oracle
@@ -5,7 +6,7 @@ from oracle import Oracle
 
 def grover(f, n, rep):
     e = etat_de_base(n-1, 1, 1) >> (H**n)
-    H_op = H_option(n, debut=0, fin=1)
+    H_op = H_option(n, debut=0, fin=-1)
     B = H_op >> (PhaseCond(n-1) @ I) >> H_op
     Uf = Oracle.phase(f)
     for i in range(rep):
@@ -19,4 +20,9 @@ def f(x, y, z):
 
 n = 3
 res = grover(f, n, 4)
+
+d = [bra(i) for i in range(2**n)]
+D = [0] * (2**n)
+
+
 print(res)
