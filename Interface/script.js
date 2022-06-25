3f875203bbdcf4e8ffb939f70e2548b91a9a0143
Firefnix <malo.leroy2662@gmail.com>
2022-06-26 01:07:23 +0200

    Interface

--- Interface/script.js
+++ Interface/script.js
@@ -1,7 +1,19 @@
-// Certains des divs n'ont pas de texte intérieur
+let circuit = [];
+
+let gatesQubits = {
+    '': 1,
+    '|0⟩': 1,
+    '|1⟩': 1,
+    'H': 1,
+    'U': NaN,
+    'cX': 2,
+    'I': 1,
+    'S': 2,
+    'X': 1
+};
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData('text', ev.target.id);
 }
 
 function allowDrop(ev) {
@@ -10,12 +22,22 @@ function allowDrop(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    let data = ev.dataTransfer.getData("text", ev.innerText);
-    ev.target.innerText = data;
+    let dest = ev.target;
+    let name = ev.dataTransfer.getData('text', ev.innerText);
+    // dest.innerText = name;
+    let x = cellRow(dest);
+    let y = cellColumn(dest);
+    console.log('Dropping', name, 'at', x, y);
+    circuit[y][x] = name;
+    if (gatesQubits[name] == 2) {
+        circuit[y+1][x] = null;
+    }
+    console.table(circuit);
+    updateGrid();
 }
 
 function clearGrid() {
-    let grid = document.getElementById("container_circuit");
+    let grid = document.getElementById('container_circuit');
     var child = grid.lastElementChild;
 
     while (child) {
@@ -24,88 +46,98 @@ function clearGrid() {
     }
 }
 
-function fill(grid, columns, rows) {
-
-    for (let i = 1; i <= columns; i++) {
-        for (let j = 1; j <= rows; j++) {
-
-            let rectangle = document.createElement("div");
-
-            rectangle.setAttribute("class", "sub_container_portes");
-            rectangle.setAttribute("id", i.toString() + "/" + j.toString());
-            rectangle.setAttribute("ondrop", "drop(event)");
-            rectangle.setAttribute("ondragover", "allowDrop(event)");
-            rectangle.style.zIndex = "10";
-
-            rectangle.style.gridColumn = i.toString() + "/" + (i+1).toString();
-            rectangle.style.gridRow = j.toString() + "/" + (j+1).toString();
-
-            grid.appendChild(rectangle);
-        }
-    }
-
-    setStates(rows);
-}
-
 function ketToString(n) {
     if (n == 0) {
-        return "|0⟩";
+        return '|0⟩';
     }
-    return "|1⟩";
+    return '|1⟩';
 }
 
-function setStates(rows) {
-    for (let j = 1; j <= rows; j++) {
-        let rect = document.getElementById("1/" + j.toString());
-        rect.innerText = ketToString(0);
-        rect.setAttribute("onclick", "switchStates(event.target)");
-        rect.setAttribute("ondrop", "");
-        rect.setAttribute("ondragover", "");
-    }
-}
-
-function switchStates(rect) {
-    let txt = rect.innerText;
-
-    if (txt == ketToString(1)) {
-        rect.innerText = ketToString(0);
-    } else {
-        rect.innerText = ketToString(1);
+function setStates(qubits) {
+    for (let i = 0; i < qubits; i++) {
+        let cell = document.getElementById(idRowColumn(0, i));
+        cell.innerText = ketToString(0);
+        circuit[i][0] = ketToString(0);
+        cell.setAttribute('onclick', 'switchState(event.target)');
+        cell.setAttribute('ondrop', '');
+        cell.setAttribute('ondragover', '');
+        // cell.setAttribute('')
     }
 }
 
-function submit() {
-    console.log("submitted")
-
-    //clearGrid();
-
-    let columnsField = document.getElementById("columns_btn");
-    let rowsField = document.getElementById("rows_btn");
-    let grid = document.getElementById("container_circuit");
-
-    let columns = columnsField.value;
-    let rows = rowsField.value;
-
-    let height = grid.offsetHeight;
-    let width = grid.offsetWidth;
-
-
-    grid.style.gridTemplateColumns = ((width/columns).toString() + "px " ).repeat(columns);
-    grid.style.gridTemplateRows = ((height/rows).toString() + "px " ).repeat(rows);
-
-    fill(grid, columns, rows);
-    links(grid, columns, rows);
+function switchState(cell) {
+    let x = cellRow(cell);
+    let y = cellColumn(cell);
+    let s = (cell.innerText == ketToString(1)) ? ketToString(0) : ketToString(1);
+    cell.innerText = s;
+    circuit[y][x] = s;
 }
 
-function links(grid, columns, rows) {
 
-    // let rect = document.createElement("div");
-    // rectangle.setAttribute("class", "sub_container_portes");
-    // rect.style.zIndex = "1";
+function idRowColumn(i, j) {
+    return `r${i}_${j}`
+}
 
-    // grid.appendChild(rect);
+function cellRow(element) {
+    return parseInt(element.id.slice(1).split('_')[0], 10);
+}
 
-    // rect.style.gridColumn = "1/3";
-    // rect.style.gridRow = "1/3";
+function cellColumn(element) {
+    return parseInt(element.id.slice(1).split('_')[1], 10);
+}
 
+function updateGridSize() {
+    let qubits = parseInt(document.getElementById('qubits_input').value);
+    let steps = parseInt(document.getElementById('steps_input').value);
+    if (isNaN(qubits) || isNaN(steps)) {
+        return;
+    }
+    console.log(`Side updated: ${steps} steps, ${qubits} qubits`);
+
+    circuit = Array(qubits);
+    for (let i = 0; i < qubits; i++) {
+        circuit[i] = Array(steps).fill('');
+    }
+    updateGrid();
+}
+
+function createCell(i, j, name = '') {
+    let qubits = gatesQubits[name];
+    let el = document.createElement('div');
+    el.style.gridColumn = `${i+1} / ${i+2}`;
+    el.style.gridRow = `${j+1} / span ${qubits}`;
+    el.style.aspectRatio = (qubits == 2) ? '0.5' : '1';
+    el.style.height = (qubits == 2) ? '75%' : '50%';
+    el.setAttribute('class', 'sub_container_portes');
+    el.setAttribute('id', idRowColumn(i, j));
+    el.setAttribute('ondrop', 'drop(event)');
+    el.setAttribute('ondragover', 'allowDrop(event)');
+    el.style.zIndex = '10';
+
+    return el;
+}
+
+function updateGrid() {
+    let rows = circuit[0].length;
+    let columns = circuit.length;
+    let grid = document.getElementById('container_circuit');
+    grid.style.gridTemplateColumns = 'auto '.repeat(rows);
+    grid.style.gridTemplateRows = 'auto '.repeat(columns);
+    clearGrid();
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let name = circuit[j][i];
+            if (name != null) {
+                let cell = createCell(i, j, name);
+                cell.innerText = name;
+                grid.appendChild(cell);
+            }
+        }
+    }
+    setStates(columns);
+}
+
+function runCircuit() {
+    console.log('Running ...')
 }
